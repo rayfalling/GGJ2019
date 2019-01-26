1954461e1840f42ae76acfe75c8ca16f20539971
天澜梦 <wanghaiwe.i.1999@163.com>
2019-01-26 15:27:46 +0800

    add jump

---
+++ Assets/Scripts/Jump/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour{
+    public float JumpForce = 10f;
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if (collision.relativeVelocity.y < 0f) {
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null) {
+                Vector2 velocity = rb.velocity;
+                velocity.y = JumpForce;
+                rb.velocity = velocity;
+            }
+        }
+    }
+}
