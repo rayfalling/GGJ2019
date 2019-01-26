6f5017d05dfd394e849941e1166291a7a5752599
天澜梦 <wanghaiwe.i.1999@163.com>
2019-01-27 04:02:56 +0800

    add main & fix

--- Assets/Scripts/Main_1/MoveToPoint.cs
+++ Assets/Scripts/Main_1/MoveToPoint.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class MoveToPoint : MonoBehaviour{
+public class MoveToPoint : MonoBehaviour
+{
     private GameObject Player;
+
+    
     //public GameObject[] linkedpoints;
 
 
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
-    void Update(){ }
-
-    private void OnMouseDown(){
-        //Player.transform.Translate(this.transform.position - Player.transform.position);
-        Player.GetComponent<PlayMove>().PlayerMoveTo(this.gameObject);
+    void Update()
+    {
+        
     }
+    private void OnMouseDown()
+    {
+        if (!Player.GetComponent<PlayMove>().CanMoving && !Player.GetComponent<PlayMove>().isMapMoving)
+        {
+            //Player.transform.Translate(this.transform.position - Player.transform.position);
+            GameManagerMain.gm.ChooseCar.SetActive(true);
+            Vector3 cameraPos = Camera.main.WorldToScreenPoint(this.transform.position);
+            GameManagerMain.gm.ChooseCar.transform.position = cameraPos;
+            GameObject.Find("UIManager").GetComponent<UIManager>().SetTarget(this.gameObject);
+            //Player.GetComponent<PlayMove>().PlayerMoveTo(this.gameObject);
+        }
 
+    }
+    
     //public bool IsLinked(GameObject point)
     //{
     //    foreach(GameObject go in linkedpoints)
@@ -31,8 +45,10 @@ public class MoveToPoint : MonoBehaviour{
     //    Debug.Log("未连接");
     //    return false;
     //}
-    private void OnTriggerEnter2D(Collider2D collision){
-        if (collision.CompareTag("Player")) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
             Player.GetComponent<PlayMove>().StopMoving();
         }
     }
--- Assets/Scripts/Main_1/PlayMove.cs
+++ Assets/Scripts/Main_1/PlayMove.cs
@@ -2,40 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayMove : MonoBehaviour{
-    public float speed;
-    //public GameObject currentPoint;
+public class PlayMove : MonoBehaviour
+{
+    private float speed;
+
+    public GameObject currentPoint;
 
     private Vector2 pointpos;
     private Vector2 dir;
 
-    private bool CanMoving;
+    private float timecount;
+    private float timeused;
+    public bool CanMoving;
+    public bool isMapMoving;
 
-    void Start(){
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timecount = 0;
+        timeused = 0;
         CanMoving = false;
+        isMapMoving = false;
         pointpos = this.transform.position;
     }
 
     // Update is called once per frame
-    void Update(){
-        if (CanMoving) {
-            transform.position = Vector2.LerpUnclamped(transform.position, pointpos, Time.deltaTime);
-            Vector2 v = (Vector2) this.transform.position - pointpos;
-            if (v.magnitude <= 0.3) {
+    void Update()
+    {
+        if(CanMoving)
+        {
+            //this.transform.position = Vector2.Lerp(this.transform.position, pointpos, Time.deltaTime);
+            //this.transform.position = Vector2.LerpUnclamped(this.transform.position, pointpos, Time.deltaTime);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, pointpos, speed*Time.deltaTime);
+            Vector2 v = (Vector2)this.transform.position - pointpos;
+
+            timecount += Time.deltaTime;
+            timeused += Time.deltaTime;
+
+            if(timecount >= 0.2)
+            {
+                timecount = 0;
+                int i = Random.Range(0, 10);
+                if(i == 0)
+                {
+                    GameManagerMain.gm.BeginRoll();
+                }
+            }
+            
+            if (v.magnitude <= 0.01)
+            {
+                //Debug.Log("Arrive!!");
+                GameManagerMain.gm.ArrivePoint(currentPoint);
+                GameManagerMain.gm.UseMoney(timeused * speed * speed);
                 CanMoving = false;
             }
         }
     }
-
-    public void PlayerMoveTo(GameObject point){
-        //currentPoint = point;
-        if (!CanMoving) {
+    public void PlayerMoveTo(GameObject point, float s)
+    {
+        
+        if (!CanMoving && !isMapMoving)
+        {
             CanMoving = true;
+            timeused = 0;
+            timecount = 0;
+            currentPoint = point;
+            speed = s;
             pointpos = point.transform.position;
-        }
-    }
 
-    public void StopMoving(){
+        }
+        
+    }
+    public void StopMoving()
+    {
+        //Debug.Log("Arrive!");
         CanMoving = false;
     }
 }
